8252666ea3e12a560e70b55ef226c02babc22ec2
José Arturo <jarturogl05@gmail.com>
2019-11-09 17:46:38 -0600

    Excepciones en el registro y clases de validación

---
+++ Server/Pacman-Sevices/DBOperationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_Sevices
+{
+    public class DBOperationResult
+    {
+        public enum AddResult
+        {
+            Success,
+            NullObject,
+            UnknowFail,
+            ExistingRecord
+        }
+    }
+}
