d7d861594432bc0792906e2f4bde2ba8595c6df2
David <davisbd100@gmail.com>
2019-11-13 09:58:12 -0600

    Sonarqube
    Cambios en el codigo para que reducir practicas de sonarqube

--- Server/Pacman-Sevices/ValidarCampos.cs
+++ Server/Pacman-Sevices/ValidarCampos.cs
@@ -5,7 +5,7 @@ namespace Pacman_Sevices
 {
     public class ValidarCampos
     {
-        public enum ResultadosValidación
+        public enum ResultadosValidacion
         {
             ContraseñaInválida,
             ContraseñaVálida,
@@ -23,26 +23,26 @@ namespace Pacman_Sevices
         /// <summary>  Valida la estructura correcta de una contraseña. Debe contener por lo menos 8 caracteres, una mayúscula y un número</summary>
         /// <param name="contraseña">  contraseña.</param>
         /// <returns>Resultado de la validación</returns>
-        public ResultadosValidación ValidarPassword(string contraseña)
+        public ResultadosValidacion ValidarPassword(string contraseña)
         {
             string patrón = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,50}$";
 
             if (Regex.IsMatch(contraseña, patrón))
             {
-                return ResultadosValidación.ContraseñaVálida;
+                return ResultadosValidacion.ContraseñaVálida;
             }
-            return ResultadosValidación.ContraseñaInválida;
+            return ResultadosValidacion.ContraseñaInválida;
 
         }
 
-        public ResultadosValidación ValidarCorreo(string correo)
+        public ResultadosValidacion ValidarCorreo(string correo)
         {
             string patrón = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
             if (Regex.IsMatch(correo, patrón))
             {
-                return ResultadosValidación.CorreoVálido;
+                return ResultadosValidacion.CorreoVálido;
             }
-            return ResultadosValidación.Correoinválido;
+            return ResultadosValidacion.Correoinválido;
         }
 
     }
