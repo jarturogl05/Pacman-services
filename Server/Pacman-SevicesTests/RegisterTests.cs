6894e41b753ebc8c4c41506baa34ac364bf6afa9
José Arturo <jarturogl05@gmail.com>
2019-12-08 15:35:06 -0600

    pruebas registro

--- Server/Pacman-SevicesTests/RegisterTests.cs
+++ Server/Pacman-SevicesTests/RegisterTests.cs
@@ -6,6 +6,23 @@ namespace Pacman_Sevices.Tests
     [TestClass()]
     public class RegisterServicesTests
     {
+
+        [TestMethod()]
+        public void AddUserTestNoConection()
+        {
+
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jarturzgl5@gmail.com";
+            jugador.Nombre = "Arturo";
+            jugador.Username = "pessp7e";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");           
+            Assert.AreEqual(DBOperationResult.AddResult.SQLError, services.AddUser(jugador));
+        }
+
         [TestMethod()]
         public void AddUserTest()
         {
@@ -14,13 +31,94 @@ namespace Pacman_Sevices.Tests
             Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
             System.Random generator = new System.Random();
             HashPass hashPass = new HashPass();
-            jugador.Correo = "jarturogl05@gmail.com";
+            jugador.Correo = "jarturzgl5@gmail.com";
             jugador.Nombre = "Arturo";
-            jugador.Username = "pep777e";
+            jugador.Username = "pessp7e";
             jugador.Password = hashPass.HashPassword("ElreyOtak02");
-            jugador.Código = generator.Next(0, 999999).ToString("D6");           
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
             Assert.AreEqual(DBOperationResult.AddResult.Success, services.AddUser(jugador));
         }
+
+        [TestMethod()]
+        public void AddWrongInvalidUserTest()
+        {
+
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jarturzgl5@gmail.com";
+            jugador.Nombre = "";
+            jugador.Username = "";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
+            Assert.AreEqual(DBOperationResult.AddResult.NullObject, services.AddUser(jugador));
+        }
+
+        [TestMethod()]
+        public void SerarchExistingUserTestNoConection()
+        {
+
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jarturzgl5@gmail.com";
+            jugador.Nombre = "Arturo";
+            jugador.Username = "pessp7e";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
+            Assert.AreEqual(DBOperationResult.AddResult.SQLError, services.SerachUserInDB(jugador));
+        }
+
+
+        [TestMethod()]
+        public void SerarchExistingUserTest()
+        {
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jarturzgl5@gmail.com";
+            jugador.Nombre = "Arturo";
+            jugador.Username = "pessp7e";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
+            Assert.AreEqual(DBOperationResult.AddResult.ExistingRecord, services.SerachUserInDB(jugador));
+        }
+
+
+
+        [TestMethod()]
+        public void SerarchNewUserTest()
+        {
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jargl5@gmail.com";
+            jugador.Nombre = "Arturo";
+            jugador.Username = "p7e";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
+            Assert.AreEqual(DBOperationResult.AddResult.Success, services.SerachUserInDB(jugador));
+        }
+
+        [TestMethod()]
+        public void SerarchInvalidUserTest()
+        {
+            Pacman_Sevices.Services services = new Services();
+            Pacman_Sevices.IRegisterService.Jugador jugador = new IRegisterService.Jugador();
+            System.Random generator = new System.Random();
+            HashPass hashPass = new HashPass();
+            jugador.Correo = "jargl5@gmail.com";
+            jugador.Nombre = "Arturo";
+            jugador.Username = "";
+            jugador.Password = hashPass.HashPassword("ElreyOtak02");
+            jugador.Código = generator.Next(0, 999999).ToString("D6");
+            Assert.AreEqual(DBOperationResult.AddResult.NullObject, services.SerachUserInDB(jugador));
+        }
+
 
     }
 }
