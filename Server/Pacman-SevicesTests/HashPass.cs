0eb0e6a1a77881d81715964a263ac2ec027acb1a
José Arturo <jarturogl05@gmail.com>
2019-12-08 13:46:03 -0600

    Entorno de pruebas

---
+++ Server/Pacman-SevicesTests/HashPass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_SevicesTests
+{
+    public class HashPass
+    {
+        public String HashPassword(String data)
+        {
+            SHA1 sha = SHA1.Create();
+            byte[] hashData = sha.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder stringBuilderValue = new StringBuilder();
+
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                stringBuilderValue.Append(hashData[i].ToString());
+            }
+            return stringBuilderValue.ToString();
+        }
+    }
+}
