04529ea15b6bdaf964fe3fef9a54d5f8ebf10845
David <davisbd100@gmail.com>
2019-12-08 22:42:42 -0600

    Pruebas de puntaje

---
+++ Server/Pacman-SevicesTests/ScoreTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pacman_Sevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_Sevices.Tests
+{
+    [TestClass()]
+    public class ScoreTests
+    {
+        [TestMethod()]
+        public void GetScoreTest()
+        {
+            Services services = new Services();
+            IScoreService.User user = new IScoreService.User();
+            user.Nombre = "bestricks";
+            user.Puntuación = 2;
+            Assert.AreEqual(2, services.GetScore(user));
+        }
+        [TestMethod()]
+        public void SetScoreTestLess()
+        {
+            Services services = new Services();
+            IScoreService.User user = new IScoreService.User();
+            user.Nombre = "bestricks";
+            user.Puntuación = 2;
+            Assert.AreEqual(false, services.SetScore(user, 2));
+        }
+        [TestMethod()]
+        public void SetScoreTestGreater()
+        {
+            Services services = new Services();
+            IScoreService.User user = new IScoreService.User();
+            user.Nombre = "bestricks";
+            user.Puntuación = 4;
+            Assert.AreEqual(true, services.SetScore(user, 2));
+        }
+    }
+}
