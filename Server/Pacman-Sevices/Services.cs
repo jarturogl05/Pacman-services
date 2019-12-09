1bbdf25e7caec0a6f0bad74cc26b7fdeb7da99c1
David <davisbd100@gmail.com>
2019-12-09 13:51:52 -0600

    Reorganizacion de codigo

--- Server/Pacman-Sevices/Services.cs
+++ Server/Pacman-Sevices/Services.cs
@@ -11,57 +11,6 @@ using System.ServiceModel;
 
 namespace Pacman_Sevices
 {
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
-    public partial class Services : IChatService
-    {
-        List<ServerUser> users = new List<ServerUser>();
-        int nextId = 1;
-        public int Connect(String name)
-        {
-            ServerUser user = new ServerUser()
-            {
-                ID = nextId,
-                Name = name,
-                operationContext = OperationContext.Current
-            };
-            nextId++;
-            Console.WriteLine("El id: " + user.ID + "\n El usuario: " + user.Name);
-            users.Add(user);
-            return user.ID;
-        }
-        public void Disconnect(int id)
-        {
-            var user = users.FirstOrDefault(i => i.ID == id);
-            if (user != null)
-            {
-                users.Remove(user);
-                SendMsg(": " + user.Name + " disconnected from chat!", 0);
-            }
-        }
-        public void SendMsg(String message, int id)
-        {
-            foreach (var item in users)
-            {
-                string answer = DateTime.Now.ToShortTimeString();
-                var user = users.FirstOrDefault(i => i.ID == id);
-                if (user != null)
-                {
-                    answer += ": " + user.Name + " ";
-                }
-                answer += message;
-                try
-                {
-                    item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
-                }
-                catch (Exception)
-                {
-                    throw new NotImplementedException();
-                }
-            }
-        }
-    }
-
-
     /// <summary>Clase parcial que contiene los métodos para realizar un registro</summary>
     /// <seealso cref="Pacman_Sevices-services.IRegisterService" />
     public partial class Services : IRegisterService
@@ -458,6 +407,59 @@ namespace Pacman_Sevices
             }
  
             return result;
+        }
+    }
+
+
+
+    /// <summary>Clase parcial que contiene los métodos para el chat</summary>
+    /// <seealso cref="Pacman_Sevices-services.IChatServices" />
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public partial class Services : IChatService
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(String name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+            users.Add(user);
+            return user.ID;
+        }
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(": " + user.Name + " disconnected from chat!", 0);
+            }
+        }
+        public void SendMsg(String message, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += message;
+                try
+                {
+                    item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
+                }
+                catch (NullReferenceException)
+                {
+                    throw new NullReferenceException("No hay jugador con ID");
+                }
+            }
         }
     }
 }
